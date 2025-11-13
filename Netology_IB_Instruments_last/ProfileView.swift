e7116b270777e73cd38cf9886f27306bee19517e
Panfilova Evgeniya <mrs.kjane@gmail.com>
2025-11-13 19:15:57 +0300

    comlited_hw

---
+++ Netology_IB_Instruments_last/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments_last
+//
+//  Created by Евгения Панфилова on 13.11.2025.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
